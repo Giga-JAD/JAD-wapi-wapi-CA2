9795c355aea775c766b29b59d35757d4eb0a5336
xR4F4ELx <rafael_macam@yahoo.com>
2025-02-10 03:54:58 +0800

    Merge branch 'main' of https://github.com/Giga-JAD/JAD-wapi-wapi-CA2.git into Rafael

--- src/main/java/com/Giga_JAD/Wapi_Wapi/controller/BookingController.java
+++ src/main/java/com/Giga_JAD/Wapi_Wapi/controller/BookingController.java
@@ -5,12 +5,15 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Giga_JAD.Wapi_Wapi.model.blueprint.Booking;
 import com.Giga_JAD.Wapi_Wapi.model.blueprint.BookingService;
 import com.Giga_JAD.Wapi_Wapi.utils.UnauthorizedException;
 
@@ -22,6 +25,26 @@ public class BookingController {
     @Autowired
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
+    }
+   
+
+    // GET /bookings/{bookingId}
+    @GetMapping("/{bookingId}")
+    public ResponseEntity<Booking> getBooking(@PathVariable Long bookingId) {
+        return bookingService.getBookingById(bookingId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // PUT /bookings/{bookingId}/status
+    @PutMapping("/{bookingId}/status")
+    public ResponseEntity<Booking> updateBookingStatus(
+            @PathVariable Long bookingId,
+            @RequestParam Integer statusId) {
+
+        return bookingService.updateBookingStatus(bookingId, statusId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{bookingId}/status")
