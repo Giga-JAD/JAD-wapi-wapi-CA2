156c19cbfe4007103fc75ff879125d11bb02ec88
Jack-wmo-std <wmoo.23@ichat.sp.edu.sg>
2025-02-09 16:50:27 +0800

    2

--- src/main/java/com/Giga_JAD/Wapi_Wapi/EnvConfig.java
+++ src/main/java/com/Giga_JAD/Wapi_Wapi/EnvConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EnvConfig {
 
-	private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing() // Ignore missing .env
+	private static final Dotenv dotenv = Dotenv.configure().directory("./").ignoreIfMissing() // Ignore missing .env
 			.load();
 
 	@Bean
